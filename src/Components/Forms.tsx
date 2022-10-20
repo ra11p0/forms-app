928368f8a40e1faa1f9e508f467c61f7d1d8a7e2
ra11p0 <p.lenczewski@icloud.com>
2022-10-20 23:54:33 +0200

    added provider

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -11,6 +11,8 @@ import ReduxSetFields from "../Redux/ReduxSet/ReduxSetFields";
 import Filler from "./Filler/Filler";
 import { useEffect } from "react";
 import FormFilledResult from "../Interfaces/Common/FormFilledResult";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18n/config";
 
 
 type Props = {
@@ -27,23 +29,25 @@ function Forms(props: Props) {
 
   return (
     <Provider store={store}>
-      <div className="w-50">
-        <Loading isReady={true}>
-          <>
-            {props.mode == ApplicationModes.Edit &&
-              <Editor
-                onSubmit={props.onSubmit}
-                onDiscard={props.onDiscard} />
-            }
-            {
-              (props.mode == ApplicationModes.Fill || props.mode == ApplicationModes.Preview) &&
-              <Filler onSubmit={props.onSubmit}
-                mode={props.mode}
-              />
-            }
-          </>
-        </Loading>
-      </div>
+      <I18nextProvider i18n={i18n}>
+        <div className="w-50">
+          <Loading isReady={true}>
+            <>
+              {props.mode == ApplicationModes.Edit &&
+                <Editor
+                  onSubmit={props.onSubmit}
+                  onDiscard={props.onDiscard} />
+              }
+              {
+                (props.mode == ApplicationModes.Fill || props.mode == ApplicationModes.Preview) &&
+                <Filler onSubmit={props.onSubmit}
+                  mode={props.mode}
+                />
+              }
+            </>
+          </Loading>
+        </div>
+      </I18nextProvider>
     </Provider>
   );
 }
