4ebcb8c1ca5365369637715e50e1b3d9d6dec1fc
ra11p0 <p.lenczewski@icloud.com>
2022-10-21 21:46:28 +0200

    build

--- dist/Components/Forms.js
+++ dist/Components/Forms.js
@@ -10,13 +10,15 @@ import ApplicationModes from "../Constraints/ApplicationModes";
 import ReduxSetFields from "../Redux/ReduxSet/ReduxSetFields";
 import Filler from "./Filler/Filler";
 import { useEffect } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18n/config";
 function Forms(props) {
     useEffect(() => {
         var _a;
         ReduxSetFields((_a = props.fields) !== null && _a !== void 0 ? _a : []);
     }, [props.fields]);
-    return (_jsx(Provider, Object.assign({ store: store }, { children: _jsx("div", Object.assign({ className: "w-50" }, { children: _jsx(Loading, Object.assign({ isReady: true }, { children: _jsxs(_Fragment, { children: [props.mode == ApplicationModes.Edit &&
-                            _jsx(Editor, { onSubmit: props.onSubmit, onDiscard: props.onDiscard }), (props.mode == ApplicationModes.Fill || props.mode == ApplicationModes.Preview) &&
-                            _jsx(Filler, { onSubmit: props.onSubmit, mode: props.mode })] }) })) })) })));
+    return (_jsx(Provider, Object.assign({ store: store }, { children: _jsx("div", Object.assign({ className: "w-100" }, { children: _jsx(I18nextProvider, Object.assign({ i18n: i18n }, { children: _jsx(Loading, Object.assign({ isReady: true }, { children: _jsxs(_Fragment, { children: [props.mode == ApplicationModes.Edit &&
+                                _jsx(Editor, { onSubmit: props.onSubmit, onDiscard: props.onDiscard }), (props.mode == ApplicationModes.Fill || props.mode == ApplicationModes.Preview) &&
+                                _jsx(Filler, { onSubmit: props.onSubmit, mode: props.mode })] }) })) })) })) })));
 }
 export default Forms;
