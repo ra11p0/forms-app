dafcff34332c8eb0f515cbd0e6b7abc15fdb7a46
Przemysław Lenczewski <p.lenczewski@icloud.com>
2022-12-19 17:34:17 +0100

    build fix

--- dist/i18n/config.js
+++ dist/i18n/config.js
@@ -1,19 +1,19 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 const i18n = i18next.createInstance({
-    fallbackNS: "common",
-    fallbackLng: "pl",
-    lng: "en",
-    resources: {
-        en: {
-            common: await import("./locales/en/common.json"),
-        },
-        pl: {
-            common: await import("./locales/pl/common.json"),
-        },
+  fallbackNS: "common",
+  fallbackLng: "pl",
+  lng: "en",
+  resources: {
+    en: {
+      common: await import("./locales/en/common.json"),
     },
-    ns: ["common"],
-    defaultNS: "common",
+    pl: {
+      common: await import("./locales/pl/common.json"),
+    },
+  },
+  ns: ["common"],
+  defaultNS: "common",
 });
 i18n.languages = ["en", "pl"];
 i18n.use(initReactI18next);
